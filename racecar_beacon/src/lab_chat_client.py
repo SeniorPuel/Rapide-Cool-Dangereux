67766b37d437091ed2746791ac879e349df7a239
SuperPatate974 <22jacoblambert@gmail.com>
2023-09-07 15:39:45 -0400

    Initial commit

--- racecar_beacon/src/lab_chat_client.py
+++ racecar_beacon/src/lab_chat_client.py
@@ -2,9 +2,28 @@
 
 import socket
 
+ 
+
 HOST = '127.0.0.1'
 PORT = 65432
+
+ 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 
+ 
+
+print("Connected to server")
+
+ 
+
+while True:
+    message = input("Client > ")
+    s.sendall(message.encode())
+    data = s.recv(1024).decode()
+    print("Server: " + data)
+
+ 
+
+s.close()
--- racecar_beacon/src/lab_chat_server.py
+++ racecar_beacon/src/lab_chat_server.py
@@ -2,9 +2,37 @@
 
 import socket
 
+ 
+
 HOST = '127.0.0.1'
 PORT = 65432
 
+ 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
+s.listen()
+
+ 
+
+print("Server is listening on {}:{}".format(HOST, PORT))
+
+ 
+
+conn, addr = s.accept()
+print("Connected by", addr)
+
+ 
+
+while True:
+    data = conn.recv(1024).decode()
+    if not data:
+        break
+    print("Client: " + data)
+    message = input("Server > ")
+    conn.sendall(message.encode())
+
+ 
+
+conn.close()
 
