cc95921edcc45b24b7d80302bc5650cc9444cfce
Pierre-Oivier Dupont <pierre-olivier.dupont@krag.io>
2023-09-10 17:14:08 -0400

    Vehicle_Tracker

--- racecar_beacon/src/vehicle_tracker.py
+++ racecar_beacon/src/vehicle_tracker.py
@@ -10,26 +10,38 @@ PORT = 65431
 
 class VehiculeTracker:
     def __init__(self):
-        
-        # Create a socket object
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        data, addr = client_socket.recvfrom(1024)  # Adjust buffer size as needed
-        
-        self.sub_Tracker = rospy.Subscriber("/odometry/filtered", ggggg, self.Unpack)
-        print("Vehicle_Tracker node started.")
-
+        print("Vehicle_Tracker")
 
     # Unpacking Data:
-    def Unpack(self, data):
+    def Unpack():
+        # Create a socket object
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        # Bind the socket to the local host and port
+        client_socket.bind(('localhost', PORT))  # You can bind to '0.0.0.0' to listen on all available interfaces
+
+        # Listen for incoming connections
+        client_socket.listen(1)  # Allow one connection at a time
+
+        # Receive Data
+        data, addr = client_socket.recvfrom(1024)  # Adjust buffer size as needed
+        
+        # Unpack Data
         vehicle_id, x, y, theta = struct.unpack('fffI', data)
+        
+        # Debug
         print("(X, Y, Theta, ID): {:.2f}, {:.2f}, {:.2f}, {:.0f}".format(x, y, theta, vehicle_id))
 
 
 if __name__ == "__main__":
-    rospy.init_node("pos_poll")
+    rospy.init_node("Vehicle_Tracker")
     node = VehiculeTracker()
     rospy.spin()
+    
+    
+    
+    
+    
     
     
     
