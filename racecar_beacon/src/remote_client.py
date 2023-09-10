6b60b2d953c99be3949758f38904ee0dbe91c2fb
SuperPatate974 <22jacoblambert@gmail.com>
2023-09-10 13:39:57 -0400

    Added new feature

--- racecar_beacon/src/remote_client.py
+++ racecar_beacon/src/remote_client.py
@@ -1,8 +1,51 @@
 #!/usr/bin/env python
 
+import rospy
+from racecar_beacon.srv import MyService, MyServiceRequest
+
+def client():
+    rospy.wait_for_service('add_numbers')
+    try:
+        add_numbers = rospy.ServiceProxy('add_numbers', MyService)
+        
+        # Prompt the user for two numbers
+        a = int(input("Enter the first number: "))
+        b = int(input("Enter the second number: "))
+        
+        request = MyServiceRequest(a=a, b=b)
+        response = add_numbers(request)
+        return response.result
+    except rospy.ServiceException as e:
+        rospy.logerr(f"Service call failed: {e}")
+
+if __name__ == '__main__':
+    rospy.init_node('my_service_client')
+    result = client()
+    rospy.loginfo(f"Result: {result}")
+
+"""#!/usr/bin/env python
+
 import socket
 
 HOST = '127.0.0.1'
 # This process should listen to a different port than the PositionBroadcast client.
 PORT = 65432
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+ 
+
+print("Connected to server")
+
+ 
+
+while True:
+    message = input("Client > ")
+    s.sendall(message.encode())
+    data = s.recv(1024).decode()
+    print("Server: " + data)
+
+ 
+
+s.close()"""
--- racecar_beacon/src/ros_monitor.py
+++ racecar_beacon/src/ros_monitor.py
@@ -1,43 +1,147 @@
 #!/usr/bin/env python
 
 import rospy
+
+import rospy
+
 import socket
+
 import threading
 
+
 from nav_msgs.msg import Odometry
+
 from sensor_msgs.msg import LaserScan
 
+from tf.transformations import euler_from_quaternion
+
+from racecar_beacon.srv import MyService, MyServiceResponse
+ 
+def quaternion_to_yaw(quat):
+
+    # Uses TF transforms to convert a quaternion to a rotation angle around Z.
+
+    # Usage with an Odometry message:
+
+    #   yaw = quaternion_to_yaw(msg.pose.pose.orientation)
+
+    (roll, pitch, yaw) = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
+
+    return yaw
+
 class ROSMonitor:
+
     def __init__(self):
+
         # Add your subscriber here (odom? laserscan?):
-        # self.sub_odom = rospy.Subcriber(...)
-        # self.sub_laser = rospy.Subscriber(...)
+
+        self.sub_laser = rospy.Subscriber("/scan", LaserScan, self.scan_update)
+
+        self.sub_odo = rospy.Subscriber("/odometry/filtered", Odometry, self.odo_update)
 
         # Current robot state:
+
         self.id = 0xFFFF
+
         self.pos = (0,0,0)
+
         self.obstacle = False
 
         # Params :
+
         self.remote_request_port = rospy.get_param("remote_request_port", 65432)
+
         self.pos_broadcast_port  = rospy.get_param("pos_broadcast_port", 65431)
 
         # Thread for RemoteRequest handling:
+
         self.rr_thread = threading.Thread(target=self.rr_loop)
+
+ 
 
         print("ROSMonitor started.")
 
-    def rr_loop(self):
-        # Init your socket here :
-        # self.rr_socket = socket.Socket(...)
-        while True:
-            pass
+ 
 
-if __name__=="__main__":
-    rospy.init_node("ros_monitor")
+    def scan_update(self, msg):
 
-    node = ROSMonitor()
+        ranges = msg
 
+        print("Got msg from /scan: ", ranges)
+
+ 
+
+    def odo_update(self, odo_msg):
+
+        # Extract the position and orientation from the Odometry message
+
+        self.position = odo_msg.pose.pose.position
+
+        self.orientation = odo_msg.pose.pose.orientation
+
+ 
+
+        # Convert the orientation quaternion to yaw
+
+        yaw = quaternion_to_yaw(self.orientation)
+
+ 
+
+        # Print the extracted information
+
+        print("Position (X, Y, Theta): {:.2f}, {:.2f}, {:.2f}".format(self.position.x, self.position.y, yaw))
+
+def handle_request(req):
+    result = req.a + req.b
+    return MyServiceResponse(result)
+
+def server():
+    rospy.init_node('my_service_server')
+    s = rospy.Service('add_numbers', MyService, handle_request)
+    print("ros_monitor started.")
     rospy.spin()
 
+if __name__ == "__main__":
+    server()
+    
+#if __name__=="__main__":
+#
+ #   rospy.init_node("ros_monitor")
+  #  node = ROSMonitor()
+   # rospy.spin()
 
+"""def rr_loop(self):
+
+        # Initialize your socket for remote requests here:
+
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as rr_socket:
+
+            # Bind the socket to the specified port
+
+            rr_socket.bind((HOST, self.remote_request_port))
+
+            rr_socket.listen()
+
+ 
+
+            print("RemoteRequest server is listening on port {}".format(self.remote_request_port))
+
+ 
+
+            while True:
+
+                conn, addr = rr_socket.accept()
+
+                print("Accepted connection from", addr)
+
+ 
+
+                # Handle remote requests here
+
+                # Example: Send robot's current position upon request
+
+                response = "Current Position: {}".format(self.position)
+
+                conn.sendall(response.encode())
+
+                conn.close()"""
