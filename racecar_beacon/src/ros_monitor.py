5128f71db034d3da5ae0636dbeff1022006d11bb
loiccv123 <loiccv123@gmail.com>
2023-09-10 14:50:29 -0600

    Auto stash before merge of "master" and "origin/master"

--- racecar_beacon/src/ros_monitor.py
+++ racecar_beacon/src/ros_monitor.py
@@ -4,6 +4,7 @@ import rospy
 import socket
 import threading
 import time
+from struct import *
 from nav_msgs.msg import Odometry
 from sensor_msgs.msg import LaserScan
 from tf.transformations import euler_from_quaternion
@@ -14,16 +15,19 @@ class ROSMonitor:
         # Add your subscriber here (odom? laserscan?):
         self.sub_laser = rospy.Subscriber("/scan", LaserScan, self.scan_update)
         self.sub_odo = rospy.Subscriber("/odometry/filtered", Odometry, self.odo_update)
+        
         # Current robot state:
         self.id = 0xFFFF
-        self.pos = (0,0,0)
+        self.pos = (0,0,0) # x, y, theta
         self.obstacle = False
+        
         # Params :
         self.remote_request_port = rospy.get_param("remote_request_port", 65432)
         self.pos_broadcast_port  = rospy.get_param("pos_broadcast_port", 65431)
         self.host = '127.0.0.1'
         # Thread for RemoteRequest handling:
         self.rr_thread = threading.Thread(target=self.rr_loop)
+        self.pb_thread = threading.Thread(target=self.pb_loop)
 
         print("ROSMonitor started.")
 
@@ -33,20 +37,43 @@ class ROSMonitor:
 
     def odo_update(self, odo_msg):
         # Extract the position and orientation from the Odometry message
-        self.position = odo_msg.pose.pose.position
-        self.orientation = odo_msg.pose.pose.orientation
-        # Convert the orientation quaternion to yaw
-        yaw = quaternion_to_yaw(self.orientation)
-        # Print the extracted information
+        self.pos[0] = odo_msg.pose.pose.position.x
+        self.pos[1] = odo_msg.pose.pose.position.y  
+        self.pos[2] = quaternion_to_yaw(odo_msg.pose.pose.orientation)
         print("Position (X, Y, Theta): {:.2f}, {:.2f}, {:.2f}".format(self.position.x, self.position.y, yaw))
+
+    def pack_data(self):
+        data_format = "fffI"
+        data = pack(data_format, self.pos[0], self.pos[1], self.pos[2], self.id)
+        print("Packed data: ", data)
+        return data
+
+    def rr_loop(self):
+        # Init your socket here :
+        # self.rr_socket = socket.Socket(...)
+        
+        rospy.init_node('my_service_server')
+        s = rospy.Service('add_numbers', MyService, handle_request)
+        print("ros_monitor started.")
+        rospy.spin()
+
+        while True:
+            pass
+
+    def pb_loop(self):
+        self.pb_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.pb_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+        while True:
+            data = self.pack_data()
+            self.pb_socket.sendto(data, ('<broadcast>', self.pos_broadcast_port))
+            time.sleep(1)
+
 
 
 def quaternion_to_yaw(quat):
-    # Uses TF transforms to convert a quaternion to a rotation angle around Z.
-    # Usage with an Odometry message:
-    #   yaw = quaternion_to_yaw(msg.pose.pose.orientation)
     (roll, pitch, yaw) = euler_from_quaternion([quat.x, quat.y, quat.z, quat.w])
-    return yaw
+    return yaw        
 
 def handle_request(req):
     result = req.a + req.b
@@ -84,18 +111,16 @@ def rr_loop(self):
     print("ros_monitor started.")
     rospy.spin()
 
-    while True:
-        pass
 
-def pb_loop(self):
-    
 
-    time.sleep(1)
-    return
     
 
 if __name__ == "__main__":
-    rr_loop()
+    rospy.init_node("ros_monitor")
+
+    node = ROSMonitor()
+
+    rospy.spin()
     
 #if __name__=="__main__":
 #
