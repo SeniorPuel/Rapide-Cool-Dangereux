3f508561cef2faeaba316073f7801524c7d61c12
loiccv123 <loiccv123@gmail.com>
2023-09-10 14:59:17 -0600

    Chris vehicle tracker code

--- racecar_beacon/src/vehicle_tracker.py
+++ racecar_beacon/src/vehicle_tracker.py
@@ -1,35 +1,36 @@
 #!/usr/bin/env python
 
 import socket
-import rospy
+import threading
 import struct
 
 HOST = '127.0.0.1'
 # This process should listen to a different port than the RemoteRequest client.
 PORT = 65431
 
-class VehiculeTracker:
-    def __init__(self):
-        
-        # Create a socket object
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        data, addr = client_socket.recvfrom(1024)  # Adjust buffer size as needed
-        
-        self.sub_Tracker = rospy.Subscriber("/odometry/filtered", ggggg, self.Unpack)
-        print("Vehicle_Tracker node started.")
+message_format = struct.Struct('fffI')
 
+# Create a UDP socket for listening
+listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    # Unpacking Data:
-    def Unpack(self, data):
-        vehicle_id, x, y, theta = struct.unpack('fffI', data)
-        print("(X, Y, Theta, ID): {:.2f}, {:.2f}, {:.2f}, {:.0f}".format(x, y, theta, vehicle_id))
+# Bind the socket to a specific address and port to listen for incoming messages
+listen_address = ('', PORT)  # Use an empty string for the host to bind to all available network interfaces
+listen_socket.bind(listen_address)
 
+def unpack_data(data):
+    data_format = "fffI"
+    unpacked_data = message_format.unpack(data)
+    return unpacked_data
 
-if __name__ == "__main__":
-    rospy.init_node("pos_poll")
-    node = VehiculeTracker()
-    rospy.spin()
-    
-    
-    
+def listen_for_messages():
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    listen_socket.bind(('', PORT))
+    while True:
+        data, addr = listen_socket.recvfrom(1024)
+        unpacked_data = unpack_data(data)
+        print(f"Received data from {addr}: {unpacked_data}")
+
+my_thread = threading.Thread(target=listen_for_messages)
+
+# Start the thread
+my_thread.start()
